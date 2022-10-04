d7d9b0c094b53baca7e9c3395a9ed6562588243b
Rocio Marchetti <marchettirociob@gmail.com>
2022-10-04 12:45:10 +0200

    Ejercicio 3

--- src/app/escena.component.ts
+++ src/app/escena.component.ts
@@ -3,10 +3,12 @@ import {Component, Input} from '@angular/core'
 
 @Component({
     selector: 'app-escena',
-    templateUrl: 'escena.component.html'
+    templateUrl: 'escena.component.html',
+    styleUrls: ['escena.component.css']
 })
 
 export class EscenaComponent {
     
     @Input() storylines: StoryLine[] = [];
+
 } 
